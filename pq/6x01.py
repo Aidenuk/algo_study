a32c2beab24c8a0bbe1395137bea5f54daf6ddc8
Aidenuk <darong0307@gmail.com>
2025-06-21 20:20:29 +0900

    repush for pq

--- pq/6x01.py
+++ pq/6x01.py
@@ -16,3 +16,25 @@ print(heap[0])
 
   
 
+# Leetcode 347
+import heapq
+from collections import Counter
+
+count = Counter(nums)
+heap = []
+    
+for num,freq in count.items():
+  heapq.heappush(heap,(freq,num))
+  if len(heap) > k:
+    heapq.heappop(heap)
+print([num for freq,num in heap])
+
+# the fastest solution
+counts = Counter(nums)
+sorted_items_by_value = sorted(counts.items(), key=lambda item: item[1])[-k:]
+result = []
+
+for i in sorted_items_by_value:
+  result.append(i[0])
+
+print(result) 
