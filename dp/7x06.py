49feffaae8d71806d6a5f95ee2bedddbd53d6cf5
Aidenuk <darong0307@gmail.com>
2025-07-01 22:07:17 +0900

    Taget sum

--- dp/7x06.py
+++ dp/7x06.py
@@ -10,11 +10,23 @@ def coins(nums,amount):
   for i in nums:
     for j in range(i, amount +1):
       dp[j] += dp[j-i]
-      
  
 
 
+# LT 494
+nums = [1,1,1,1,1]
+target = 3
+def findTargetSumWays(nums,target):
+        dp = {}
 
-
-
-
+        def backtracking(i,total):
+            if i == len(nums):
+                return 1 if total == target else 0
+            
+            if (i,total) in dp:
+                return dp[(i,total)]
+            
+            dp[(i,total)] = (backtracking(i + 1, total + nums[i]) + backtracking(i+1,total-nums[i]))
+            return dp[(i,total)]
+        return backtracking(0,0)
+findTargetSumWays(nums,target)
