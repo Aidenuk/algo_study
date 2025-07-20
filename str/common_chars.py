ffb9f8b5d2299688e4481f912945b38144f5e4df
Elmurodjon <96878908+Elmurodjon01@users.noreply.github.com>
2025-07-20 20:01:13 +0900

    update

--- str/common_chars.py
+++ str/common_chars.py
@@ -1,26 +1,39 @@
+#leetcode 1002
+
+
+# def commonChars(words):
+#     common_chars = []
+#     common_chars.append(list(words)[0])
+#     result = []
+#     for i in range(len(words)):
+#         for each_char in words[i]:
+#             newlist = list(common_chars[0])
+#             if each_char in newlist:
+#                 result.append(each_char)
+
+#     print(result)
+    
+
 
 
 
 def commonChars(words):
-    common_chars = []
-    common_chars.append(list(words)[0])
+    if not words:
+        return []
+    
+    common_chars = list(words[0])
     result = []
-    for i in range(len(words)):
-        for each_char in words[i]:
-            newlist = list(common_chars[0])
-            if each_char in newlist:
-                result.append(each_char)
-
-    print(result)
+    for char in common_chars:
+        appears_in_all = True
+        for word in words:
+            if char not in word:
+                appears_in_all = False
+                break
+        if appears_in_all:
+            result.append(char)
+            for i in range(len(words)):
+                words[i] = words[i].replace(char, '', 1)
     
-        
-        
-        
-        
-    
-    
-
-
+    return result
 
 print(commonChars(['cool', 'lock', 'cook']))
-
